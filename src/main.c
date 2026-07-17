eedc12cce8d251a372bba0acdaa0ae927d598c14
Henry <henry.kaercher1@gmail.com>
2026-07-17 17:17:36 -0300

    added -r e -d options and using -- for long args now

--- src/main.c
+++ src/main.c
@@ -15,7 +15,9 @@ typedef enum{
 	ARG_CHECK_ALL,
 	ARG_CHECK_CURRENT,
 	ARG_ERROR,
-	ARG_WATCH
+	ARG_WATCH,
+	ARG_DELETE_ID,
+	ARG_DELETE_NAME
 }arg_type;
 
 typedef enum{
@@ -24,6 +26,8 @@ typedef enum{
     SQL_STOP_SESSION,
 	SQL_GET_ALL,
     SQL_GET_CURRENT,
+    SQL_DELETE_BY_ID,
+    SQL_DELETE_BY_NAME,
     SQL_QUERY_COUNT
 } sql_query;
 
@@ -45,27 +49,37 @@ static const char *sql[SQL_QUERY_COUNT] = {
 		"WHERE end IS NULL;",
 
 	[SQL_GET_ALL] =
-		"SELECT task, start, end "
+		"SELECT id, task, start, end "
 		"FROM sessions;",
 
 	[SQL_GET_CURRENT] =
 		"SELECT task, start, end "
 		"FROM sessions "
 		"WHERE end IS NULL "
-		"ORDER BY id DESC LIMIT 1;"
+		"ORDER BY id DESC LIMIT 1;",
+
+	[SQL_DELETE_BY_ID] =
+		"DELETE FROM sessions "
+		"WHERE id = ?;",
+
+	[SQL_DELETE_BY_NAME] =
+		"DELETE FROM sessions "
+		"WHERE task = ?;"
 };
 
-void db(arg_type arg, char *task_name);
+void db(arg_type arg, const char *task_name);
 arg_type arg_holder(u8 argc, char **argv);
 void print_time(time_t timestamp);
 void print_duration(time_t duration);
 void watch(sqlite3 *db);
 u8 show_current_task(sqlite3 *db);
+void delete_session(sqlite3 *db, const char *query, const char *arg);
+void delete_id(sqlite3 *db, const char *query, u32 id);
 
 u32 main(u8 argc, char **argv){
 	arg_type args = arg_holder(argc, argv);
 	if(args == ARG_ERROR){
-		fprintf(stderr, "Argument not supported, use -h or -help to see the help page.\n");
+		fprintf(stderr, "Argument not supported, use -h or --help to see the help page.\n");
 		return 1;
 	}
 
@@ -77,11 +91,13 @@ u32 main(u8 argc, char **argv){
 			"    ctimer [OPTION] [TASK]\n"
 			"\n"
 			"OPTIONS\n"
-			"    -s, -start <task>    Start a new task.\n"
-			"    -p, -stop            Stop the current task.\n"
-			"    -c, -check           Show all recorded sessions.\n"
-			"    -w, -watch           Show the current task in real time.\n"
-			"    -h, -help            Display this help message.\n"
+			"    -s, --start <task>    		Start a new task.\n"
+			"    -p, --stop            		Stop the current task.\n"
+			"    -c, --check           		Show all recorded sessions.\n"
+			"    -w, --watch           		Show the current task in real time.\n"
+			"	 -d, --delete <id>	   		Delete a session by its ID.\n"
+			"	 -r, --remove-name <task>   Remove all sessions with the same name.\n"
+			"    -h, --help            		Display this help message.\n"
 			"\n"
 			"DEFAULT BEHAVIOR\n"
 			"    Running \"ctimer\" without arguments shows the current\n"
@@ -94,18 +110,18 @@ u32 main(u8 argc, char **argv){
 		return 0;
 	}
 
-	char *task = (argc > 2) ? argv[2] : "*unnamed*";
+	const char *task = (argc > 2) ? argv[2] : "*unnamed*";
 	db(args, task);
 	return 0;
 }
 
-void db(arg_type arg, char *task_name){
+void db(arg_type arg, const char *task_name){
 	sqlite3 *db;
 	char *err;
 	char dir[522];
 	char path[522];
 
-	snprintf(dir, sizeof(path), "%s/.local/share/ctimer", getenv("HOME"));
+	snprintf(dir, sizeof(dir), "%s/.local/share/ctimer", getenv("HOME"));
 	snprintf(path, sizeof(path), "%s/ctimer.db", dir);
 	if(mkdir(dir, 0755) == -1 && errno != EEXIST){
 		perror("mkdir");
@@ -168,10 +184,12 @@ void db(arg_type arg, char *task_name){
 		sqlite3_prepare_v2(db, sql[SQL_GET_ALL], -1, &stmt, NULL);
 
 		while(sqlite3_step(stmt) == SQLITE_ROW){
-			const char *task = (const char *)sqlite3_column_text(stmt, 0);
-			sqlite3_int64 start = sqlite3_column_int64(stmt, 1);
-			sqlite3_int64 end = sqlite3_column_int64(stmt, 2);
+			sqlite3_int64 id = sqlite3_column_int64(stmt, 0);
+			const char *task = (const char *)sqlite3_column_text(stmt, 1);
+			sqlite3_int64 start = sqlite3_column_int64(stmt, 2);
+			sqlite3_int64 end = sqlite3_column_int64(stmt, 3);
 
+			printf("ID:%lld -- ", (long long)id);
 			printf("%s ", task);
 			print_time(start);
 			printf(" -> ");
@@ -187,6 +205,13 @@ void db(arg_type arg, char *task_name){
 	else if(arg == ARG_WATCH){
 		watch(db);
 	}
+	else if(arg == ARG_DELETE_ID){
+		u32 id = atoi(task_name);
+		delete_id(db, sql[SQL_DELETE_BY_ID], id);
+	}
+	else if(arg == ARG_DELETE_NAME){
+		delete_session(db, sql[SQL_DELETE_BY_NAME], task_name);
+	}
 
 	sqlite3_close(db);
 }
@@ -195,10 +220,12 @@ arg_type arg_holder(u8 argc, char **argv){
   if(argc < 2) return ARG_CHECK_CURRENT;
 
     if(strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-h") == 0) return ARG_HELP;
-	else if(strcmp(argv[1], "-start") == 0 || strcmp(argv[1], "-s") == 0) return ARG_START;
-	else if(strcmp(argv[1], "-stop") == 0 || strcmp(argv[1], "-p") == 0) return ARG_STOP;
-	else if(strcmp(argv[1], "-check") == 0 || strcmp(argv[1], "-c") == 0) return ARG_CHECK_ALL;
-	else if(strcmp(argv[1], "-watch") == 0 || strcmp(argv[1], "-w") == 0) return ARG_WATCH;
+	else if(strcmp(argv[1], "--start") == 0 || strcmp(argv[1], "-s") == 0) return ARG_START;
+	else if(strcmp(argv[1], "--stop") == 0 || strcmp(argv[1], "-p") == 0) return ARG_STOP;
+	else if(strcmp(argv[1], "--check") == 0 || strcmp(argv[1], "-c") == 0) return ARG_CHECK_ALL;
+	else if(strcmp(argv[1], "--watch") == 0 || strcmp(argv[1], "-w") == 0) return ARG_WATCH;
+	else if(strcmp(argv[1], "--delete") == 0 || strcmp(argv[1], "-d") == 0) return ARG_DELETE_ID;
+	else if(strcmp(argv[1], "--remove-name") == 0 || strcmp(argv[1], "-r") == 0) return ARG_DELETE_NAME;
 	else return ARG_ERROR;
 }
 
@@ -227,35 +254,70 @@ void print_duration(time_t duration){
 
 void watch(sqlite3 *db){
 	while(1){
-		if(show_current_task(db) == 1) break;;
+		if(show_current_task(db) == 1) break;
 		printf("\033[2J\033[H");
 		sleep(1);
 	}
 }
 
 u8 show_current_task(sqlite3 *db){
-		sqlite3_stmt *stmt;
+	sqlite3_stmt *stmt;
 
-		sqlite3_prepare_v2(db, sql[SQL_GET_CURRENT], -1, &stmt, NULL);
+	sqlite3_prepare_v2(db, sql[SQL_GET_CURRENT], -1, &stmt, NULL);
 
-		if(sqlite3_step(stmt) == SQLITE_ROW){
-			const char *task = (const char *)sqlite3_column_text(stmt, 0);
-			sqlite3_int64 start = sqlite3_column_int64(stmt, 1);
+	if(sqlite3_step(stmt) == SQLITE_ROW){
+		const char *task = (const char *)sqlite3_column_text(stmt, 0);
+		sqlite3_int64 start = sqlite3_column_int64(stmt, 1);
 
-			time_t duration = time(NULL) - start;
+		time_t duration = time(NULL) - start;
 
-			printf("%s ", task);
-			printf("\nStart: ");
-			print_time(start);
-			printf("\nDuration: ");
-			print_duration(duration);
-			printf("\n");
-		}
-		else{
-			printf("You don't have any task running. Use -h or -help if need help for using the application.\n");
-			return 1;
-		}
-
+		printf("%s ", task);
+		printf("\nStart: ");
+		print_time(start);
+		printf("\nDuration: ");
+		print_duration(duration);
+		printf("\n");
+	}
+	else{
 		sqlite3_finalize(stmt);
-		return 0;
+		printf("You don't have any task running. Use -h or --help if need help for using the application.\n");
+		return 1;
+	}
+
+	sqlite3_finalize(stmt);
+	return 0;
+}
+
+void delete_session(sqlite3 *db, const char *query, const char *arg){
+	sqlite3_stmt *stmt;
+	sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, arg, -1, SQLITE_TRANSIENT);
+
+	if(sqlite3_step(stmt) != SQLITE_DONE){
+		fprintf(stderr, "%s\n", sqlite3_errmsg(db));
+		printf("Failed to delete [%s].\n", arg);
+	}
+	else{
+		if (sqlite3_changes(db) == 0) printf("No session named [%s] found.\n", arg);
+		else printf("All sessions named [%s] have been removed.\n", arg);
+	}
+
+	sqlite3_finalize(stmt);
+}
+
+void delete_id(sqlite3 *db, const char *query, u32 id){
+	sqlite3_stmt *stmt;
+	sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id);
+
+	if(sqlite3_step(stmt) != SQLITE_DONE){
+		fprintf(stderr, "%s\n", sqlite3_errmsg(db));
+		printf("Failed to delete session with id:[%d].\n", id);
+	}
+	else{
+		if(sqlite3_changes(db) == 0) printf("No session with id [%d] found.\n", id);
+		else printf("The session with id:[%d] has been removed.\n", id);
+	}
+
+	sqlite3_finalize(stmt);
 }
